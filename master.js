566db77c5cc5f0313dac17c60a144ffe07d9559e
Alice Q Wong <alice@pelotoncycle.com>
2016-01-30 18:18:43 -0500

    working

---
+++ master.js
@@ -0,0 +1,129 @@
+var Wordnik = {
+    get: function(path){
+        var base = "http://api.wordnik.com/v4/";
+        var query_params = "?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        return $.ajax({
+            url: base+path+query_params,
+            async: false
+        });
+    }
+};
+
+function getSyllables(word){
+    if(!word){
+        return 0;
+    }
+    var path = "word.json/"+word+"/hyphenation";
+    var count = 0;
+
+    Wordnik.get(path).done(function(data){
+        count = data.length;
+    });
+
+    return count;
+}
+
+function countSyllables(line){
+    var words = line.split(/\s|\-/);
+    //TODO: strip . , ;
+    var counts = words.map(getSyllables);
+    return counts.reduce(function(a, b){ return a+b; }, 0);
+}
+
+function Line(targetCount){
+    this.targetCount = 0;
+    this.leeway = 0;
+    this.runningCount = 0;
+    this.raw = '';
+    if(targetCount){
+        this.targetCount = targetCount;
+    }
+}
+
+Line.prototype.percent = function(){
+    if(this.targetCount===0){
+        return '0';
+    }
+    var p = (this.runningCount/this.targetCount)*10;
+    return (parseInt(Math.floor(p))*10)+'';
+}
+
+Line.prototype.status = function(){
+    if(this.targetCount==0){
+        return '';
+    } else if (this.runningCount > this.targetCount) {
+        return 'has-error';
+    } else if (this.runningCount < this.targetCount) {
+        return 'has-warning';
+    } else {
+        return 'has-success';
+    }
+}
+
+var app = angular.module('haiku', []);
+app.controller('Ctrl', ['$scope', function($scope){
+
+    $scope.lines = [new Line()];
+
+    $scope.newLine = function(copyLast){
+
+        var lastLine = $scope.lines[this.lines.length-1];
+        if(lastLine.raw === '') {
+            return;
+        }
+
+        var target = 0;
+        if (copyLast) {
+            target = lastLine.targetCount;
+        }
+        var newLine = new Line(target);
+        $scope.lines.push(newLine);
+    };
+
+    $scope.onLineChange = function(line){
+        line.runningCount = countSyllables(line.raw);
+
+        $scope.newLine(true);
+    };
+
+    $scope.download = function() {
+        var rawLines = $scope.lines.map(function(e){ return e.raw; });
+        var text = rawLines.join('\n');
+
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', 'lyrics.txt');
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    }
+
+
+}]);
+
+app.directive('lineTarget', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            line: '=line'
+        },
+        link: function (scope, element) {
+            scope.editing = false;
+
+            scope.changeTarget = function(){
+                scope.editing = true;
+            };
+
+            scope.saveTarget =  function(){
+                scope.editing = false;
+            }
+        },
+        transclude: true,
+        templateUrl: 'templates/target_count.html'
+        
+    };
+});
